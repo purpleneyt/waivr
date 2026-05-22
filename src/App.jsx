d66e8f8b9c215dffbb63c8e1dec0a2d62912ab8d
Nate <natelibao71@gmail.com>
2026-05-22 21:25:43 +0800

    fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './styles/global.css'
 import PhoneFrame from './components/PhoneFrame'
 import BottomNav from './components/BottomNav'
@@ -19,29 +19,40 @@ import Notifications from './pages/Notifications'
 import QR from './pages/QR'
 import Profile from './pages/Profile'
 
+function AppContent() {
+  const location = useLocation()
+  const hiddenNavRoutes = ['/']
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/send-money" element={<SendMoney />} />
+        <Route path="/payment-mode" element={<PaymentMode />} />
+        <Route path="/ad-loading" element={<AdLoading />} />
+        <Route path="/ad-playback" element={<AdPlayback />} />
+        <Route path="/ad-verified" element={<AdVerified />} />
+        <Route path="/transfer-processing" element={<TransferProcessing />} />
+        <Route path="/transfer-success" element={<TransferSuccess />} />
+        <Route path="/transaction-history" element={<TransactionHistory />} />
+        <Route path="/sponsored-faq" element={<SponsoredFAQ />} />
+        <Route path="/high-risk-session" element={<HighRiskSession />} />
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/qr" element={<QR />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      {!hiddenNavRoutes.includes(location.pathname) && <BottomNav />}
+    </>
+  )
+}
+
 function App() {
   return (
     <BrowserRouter>
       <PhoneFrame>
-        <Routes>
-          <Route path="/" element={<SplashScreen />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/send-money" element={<SendMoney />} />
-          <Route path="/payment-mode" element={<PaymentMode />} />
-          <Route path="/ad-loading" element={<AdLoading />} />
-          <Route path="/ad-playback" element={<AdPlayback />} />
-          <Route path="/ad-verified" element={<AdVerified />} />
-          <Route path="/transfer-processing" element={<TransferProcessing />} />
-          <Route path="/transfer-success" element={<TransferSuccess />} />
-          <Route path="/transaction-history" element={<TransactionHistory />} />
-          <Route path="/sponsored-faq" element={<SponsoredFAQ />} />
-          <Route path="/high-risk-session" element={<HighRiskSession />} />
-          <Route path="/inbox" element={<Inbox />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/qr" element={<QR />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-        <BottomNav />
+        <AppContent />
       </PhoneFrame>
     </BrowserRouter>
   )
